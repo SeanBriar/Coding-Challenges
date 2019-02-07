2d48d762c98ed0b1f96d328ca448ddb302c4c632
Sean <seanoneill@Bons-MacBook-Pro.local>
2019-02-07 13:32:17 -0800

    feb 7th

---
+++ February/squares_sorted_array.js
@@ -0,0 +1,18 @@
+const array = [-4,-1,0,3,10]
+
+const sortedSquares = (array)=>{
+  let newArr = []
+  // console.log(array);
+  array.forEach((i)=>{
+    let s = i * i
+    // console.log(a);
+    newArr.push(s)
+    newArr.sort((a, b) =>  a - b)
+    return newArr;
+  })
+
+  // console.log(newArr);
+
+}
+
+sortedSquares(array)
